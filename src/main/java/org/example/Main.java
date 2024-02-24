99c343ea3826180546f47567440d3eac6cbb73fd
dongwonbyun <98743428+dongwonbyun@users.noreply.github.com>
2024-02-24 13:07:54 +0900

    Update Main.java
    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!1");
-        System.out.println("Hello world! dev0");
+        System.out.println("Hello world! f_001");
         System.out.println("Hello world! dev1");
     }
 }
