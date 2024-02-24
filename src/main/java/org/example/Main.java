c4256beca3ee259ad29bfc8b969c21ba896fbe9b
dongwonbyun <dwon.byun@gmail.com>
2024-02-24 12:45:06 +0900

    add text

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!1");
-        System.out.println("Hello world! dev");
+        System.out.println("Hello world! dev0");
+        System.out.println("Hello world! dev1");
     }
 }
